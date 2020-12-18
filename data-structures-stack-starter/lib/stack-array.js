79187dd3f01b5a62b5cf1744f1a6ed1980b6ecd6
Peter Anderson <peterburtonanderson@gmail.com>
2020-12-18 14:22:33 -0700

    stack practice problems complete

---
+++ data-structures-stack-starter/lib/stack-array.js
@@ -0,0 +1,25 @@
+
+class Array {
+    constructor() {
+
+    }
+}
+
+
+class Stack {
+    constructor() {
+        this.elements = [];
+    }
+
+    push(el) {
+        this.elements.push(el);
+    }
+
+    pop() {
+
+    }
+
+    size() {
+        
+    }
+}
--- data-structures-stack-starter/lib/stack.js
+++ data-structures-stack-starter/lib/stack.js
@@ -21,11 +21,49 @@
 // -----------
 
 class Node {
-
+    constructor(value) {
+        this.value = value;
+        this.next = null;
+    }
 }
 
 class Stack {
+    constructor() {
+        this.top = null;
+        this.length = 0;
+    }
 
+    push(val) {
+        let newNode = new Node(val);
+        if (this.length === 0) {
+            this.top = newNode;
+        } else {
+            let oldNode = this.top;
+            newNode.next = oldNode;
+            this.top = newNode;
+        }
+        this.length++
+        return this.length;
+    }
+
+    pop() {
+        let currTop = this.top;
+        if (this.top === null) {
+            return null;
+        } 
+        else if (this.top.next === null) {
+            this.top = null;
+        } 
+        else {
+            this.top = this.top.next;
+        } 
+        this.length--
+        return currTop.value;
+    }
+
+    size() {
+        return this.length;
+    }
 }
 
 exports.Node = Node;
